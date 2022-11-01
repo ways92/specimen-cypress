14360ee425fcecb1b739854ca7ffcf2ec15d78b1
Ways Alqorni <wayslos92@gmail.com>
2022-11-01 23:02:03 +0700

    add cucumber

--- cypress/integration/uploadFile/uploadFile.spec.ts
+++ cypress/integration/uploadFile/uploadFile.spec.ts
@@ -1,9 +1,10 @@
 describe('Upload file', () => {
     it( 'upload file valid data', () =>{
-    const filePath: string = 'upload1.png'
-    cy.visit('https://the-internet.herokuapp.com/upload')
-    cy.get('#file-upload').attachFile(filePath)
-    cy.get( '#file-submit' ).click()
-    cy.get('#uploaded-files').contains(filePath)
+      const filePath: string = 'upload1.png'
+      cy.visit('https://the-internet.herokuapp.com/upload')
+      cy.get('#file-upload').attachFile(filePath)
+      cy.get( '#file-submit' ).click()
+      cy.get('#uploaded-files').contains(filePath)
+      // cy.get('#uploaded-files').should('be.visible')
   })
 })
